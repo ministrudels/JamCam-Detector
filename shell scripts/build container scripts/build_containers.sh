ade33bf2e705d7ca3e808b1a0de40bb33a1f5c5c
Huan Min Gan <ganhuanmin@gmail.com>
2019-09-04 22:06:51 +0100

    Rename file directories

--- shell scripts/build container scripts/build_containers.sh
+++ shell scripts/build container scripts/build_containers.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-#printf "\nBuilding download video to volume container\n"
-#docker build --tag download_video_to_volume "docker containers/download video to volume/."
+#printf "\nBuilding download_video_to_volume container\n"
+#docker build --tag download_video_to_volume "docker_containers/download_video_to_volume/."
 #
 #printf "\nBuilding output detected video container\n"
-#docker build --tag output_detected_video "docker containers/output_detected_video/."
+#docker build --tag output_detected_video "docker_containers/output_detected_video/."
 #
-#printf "\nBuilding upload video container\n"
-#docker build --tag upload_video "docker containers/upload video/."
+#printf "\nBuilding upload_video container\n"
+#docker build --tag upload_video "docker_containers/upload_video/."
 
 
 printf "\nBuilding download videos container\n"
-docker build --tag store_videos "docker containers/store videos/."
+docker build --tag store_videos "docker_containers/store_videos/."
 
 printf "\nBuilding inputfeeds_detection container\n"
-docker build --tag inputfeeds_detection "docker containers/Input feeds from detectionAPI/."
+docker build --tag inputfeeds_detection "docker_containers/input_from_API/."
 
 printf "\nBuilding log parser container\n"
-docker build --tag parse_dn_checksum "docker containers/Parse darknet logs/."
+docker build --tag parse_dn_checksum "docker_containers/parse_dn_logs/."
